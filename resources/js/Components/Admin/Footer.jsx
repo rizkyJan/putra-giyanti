f1fb017ff4a27a73217c64fa6bbf1f57f946d507
rizkyJan <l200220146@student.ums.ac.id>
2026-08-10 05:42:39 +0700

    update footer admin

--- resources/js/Components/Admin/Footer.jsx
+++ resources/js/Components/Admin/Footer.jsx
@@ -1,8 +1,8 @@
 export default function Footer() {
     return (
         <footer className="bg-white p-4 text-center text-sm text-gray-500 shadow-inner mt-auto">
-            &copy; {new Date().getFullYear()} Karang Taruna. Dibuat dengan React
-            & Laravel.
+            &copy; {new Date().getFullYear()} Putra Giyanti APP. all rights
+            reserved.
         </footer>
     );
 }
